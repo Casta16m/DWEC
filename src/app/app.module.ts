21d1621f48e1cb3deff2d4974d51412be11536de
Casta16m <102661672+Casta16m@users.noreply.github.com>
2023-03-07 16:41:35 +0100

    Taulell casi creat

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
+import { AppRoutingModule } from './app-routing.module';
+import { EntrarEquipComponent } from './components/entrar-equip/entrar-equip.component';
+import { JugarComponent } from './components/jugar/jugar.component';
+import { TaulellComponent } from './components/taulell/taulell.component';
+const config: SocketIoConfig = { url: 'http://localhost:4444', options: {} };
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EntrarEquipComponent,
+    JugarComponent,
+    TaulellComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    SocketIoModule.forRoot(config),
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
